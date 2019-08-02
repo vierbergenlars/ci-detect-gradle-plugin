1f80248803b7623e8602e785cb7eea50ca1d08fc
Lars Vierbergen <lars@vbgn.be>
2019-08-02 17:37:32 +0200

    Fix locking in CiInformationProviderImpl

--- src/main/java/be/vbgn/gradle/cidetect/provider/CiInformationProviderImpl.java
+++ src/main/java/be/vbgn/gradle/cidetect/provider/CiInformationProviderImpl.java
@@ -4,18 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ServiceLoader;
 
-class CiInformationProviderImpl {
+final class CiInformationProviderImpl {
+
     private static List<CiInformationProvider> installedProviders = null;
 
-    public static List<CiInformationProvider> installedProviders() {
-        if(installedProviders == null) {
-            ServiceLoader<CiInformationProvider> serviceLoader = ServiceLoader.load(
-                    CiInformationProvider.class);
+    private static final Object lock = new Object();
 
-            installedProviders = new ArrayList<>();
+    private CiInformationProviderImpl() {
+    }
 
-            for (CiInformationProvider ciInformationProvider : serviceLoader) {
-                installedProviders.add(ciInformationProvider);
+
+    static List<CiInformationProvider> installedProviders() {
+        if (installedProviders == null) {
+            ServiceLoader<CiInformationProvider> serviceLoader = ServiceLoader.load(CiInformationProvider.class);
+
+            synchronized (lock) {
+                installedProviders = new ArrayList<>();
+
+                for (CiInformationProvider ciInformationProvider : serviceLoader) {
+                    installedProviders.add(ciInformationProvider);
+                }
             }
         }
         return installedProviders;
